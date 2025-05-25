59f1f92a9926d2f54a58f14e984998b953ba421a
lidor katzav <Lidorka@ac.sce.ac.il>
2025-05-25 16:13:20 +0300

    trainer prodile firestore

--- lib/modules/trainee/features/trainer-profile/data/repositories/firestore_trainerprofile_repository.dart
+++ lib/modules/trainee/features/trainer-profile/data/repositories/firestore_trainerprofile_repository.dart
@@ -2,6 +2,9 @@ import 'package:studiosync/core/data/data_source/firebase/firestore_service.dart
 import 'package:studiosync/modules/trainee/features/trainer-profile/domain/repositories/i_trainer_profile_repository.dart';
 import 'package:studiosync/modules/trainer/features/notifications/data/models/request_model.dart';
 
+
+
+
 class TrainerProfileFirestoreRepository implements ITrainerProfileRepository {
   final FirestoreService firestoreService;
 
