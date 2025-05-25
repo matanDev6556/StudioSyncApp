dcb349f11775934e06b8cf7c19d15360751443e4
lidor katzav <Lidorka@ac.sce.ac.il>
2025-05-25 16:16:56 +0300

    send req for trainer use case

--- lib/modules/trainee/features/trainer-profile/domain/usecases/send_request_usecase.dart
+++ lib/modules/trainee/features/trainer-profile/domain/usecases/send_request_usecase.dart
@@ -2,6 +2,7 @@ import 'package:studiosync/modules/trainee/features/profile/data/models/trainee_
 import 'package:studiosync/modules/trainee/features/trainer-profile/domain/repositories/i_trainer_profile_repository.dart';
 import 'package:studiosync/modules/trainer/features/notifications/data/models/request_model.dart';
 
+// this usecase is used to send a request to a trainer
 class SendRequestUseCase {
   final ITrainerProfileRepository repository;
 
