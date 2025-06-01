ba63f2cabf0493d15626a4d17bef80c82e55358a
lidor katzav <Lidorka@ac.sce.ac.il>
2025-06-01 16:12:52 +0300

    my trainer repo

--- lib/modules/trainee/features/profile/data/repositories/firestore_mytrainer_repository.dart
+++ lib/modules/trainee/features/profile/data/repositories/firestore_mytrainer_repository.dart
@@ -3,7 +3,6 @@ import 'package:studiosync/modules/trainee/features/profile/data/models/trainee_
 import 'package:studiosync/modules/trainee/features/profile/domain/repositories/i_mytrainer_repository.dart';
 import 'package:studiosync/modules/trainer/features/profile/data/models/trainer_model.dart';
 
-
 class MyTrainerFirestoreRepository implements IMyTrainerRepositroy {
   final FirestoreService _firestoreService;
 
